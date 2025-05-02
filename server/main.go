fec75309a3614185a30adf68052d5f5b430d00c4
Thitiphum <stacktemple@gmail.com>
2025-05-02 23:11:45 +0700

    implement centralized config loader with .env and fallback to system env

--- server/main.go
+++ server/main.go
@@ -4,14 +4,21 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/stacktemple/realtime-chat/server/config"
 )
 
 func main() {
+
+	config.Init()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, StackTemple!")
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	println(config.Cfg.DatabaseURL)
+	println(config.Cfg.JWTSecret)
+
+	log.Fatal(app.Listen(":" + config.Cfg.Port))
 }
