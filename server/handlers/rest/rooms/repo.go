e0ba88a23702b37efe54aa39482ecb94fc195237
Thitiphum <stacktemple@gmail.com>
2025-05-07 04:45:44 +0700

    room repository

---
+++ server/handlers/rest/rooms/repo.go
@@ -0,0 +1,55 @@
+package rooms
+
+import (
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type RoomRepo struct {
+	DB *sqlx.DB
+}
+
+func (r *RoomRepo) RoomExistsToday(name string) (bool, error) {
+	var exists bool
+	query := `SELECT EXISTS (
+		SELECT 1 FROM rooms WHERE name = $1 AND created_date = CURRENT_DATE
+	)`
+	err := r.DB.Get(&exists, query, name)
+	return exists, err
+}
+
+func (r *RoomRepo) CreateRoom(name string, passwordHash *string, needPass bool) (string, error) {
+	var roomID string
+	query := `
+		INSERT INTO rooms (name, password_hash, need_pass, created_date)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id`
+	err := r.DB.Get(&roomID, query, name, passwordHash, needPass, time.Now().Format("2006-01-02"))
+	return roomID, err
+}
+
+func (r *RoomRepo) AddGuest(roomID string, guestName string, isOwner bool) error {
+	query := `
+		INSERT INTO room_guests (room_id, guest_name, is_owner)
+		VALUES ($1, $2, $3)`
+	_, err := r.DB.Exec(query, roomID, guestName, isOwner)
+	return err
+}
+
+type RoomInfo struct {
+	Name      string    `db:"name" json:"name"`
+	NeedPass  bool      `db:"need_pass" json:"need_pass"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+}
+
+func (r *RoomRepo) GetTodayRooms() ([]RoomInfo, error) {
+	var rooms []RoomInfo
+	query := `
+		SELECT name, need_pass, created_at
+		FROM rooms
+		WHERE created_date = CURRENT_DATE
+		ORDER BY created_at DESC`
+	err := r.DB.Select(&rooms, query)
+	return rooms, err
+}
