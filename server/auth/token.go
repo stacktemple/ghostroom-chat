41a12b2a5547563785cb5db76bd1a39e0c1676f7
Thitiphum <stacktemple@gmail.com>
2025-05-07 11:14:58 +0700

    Add ParseToken function and enhance CreateToken

--- server/auth/token.go
+++ server/auth/token.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"maps"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -12,12 +13,32 @@ func CreateToken(secret string, claims map[string]any, expiryHours int) (string,
 		"iat": time.Now().Unix(),
 	}
 
-	for k, v := range claims {
-		stdClaims[k] = v
-	}
+	maps.Copy(stdClaims, claims)
 
 	// Create token with HS256
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, stdClaims)
 
 	return token.SignedString([]byte(secret))
+}
+
+func ParseToken(secret string, tokenStr string) (map[string]any, error) {
+	parser := jwt.NewParser(jwt.WithLeeway(1 * time.Minute))
+
+	token, err := parser.Parse(tokenStr, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrTokenSignatureInvalid
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// get claims map
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrTokenInvalidClaims
+	}
+
+	return claims, nil
 }
